4367f46ccd2c42362fc243625c04b0d92af2ad6a
Jasmin Hidanovic <130430763+dexinho@users.noreply.github.com>
2023-09-16 22:49:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const searchBar = document.querySelector('#search-bar')
+const pageCount = document.querySelector('#page-count')
+const backwardOrForward = document.querySelectorAll('.backward-or-forward')
